c6446fc43f18dac74a1d67d7dce59c67e750728b
Jeff Ripke <jripke74@gmail.com>
2017-09-05 19:00:19 -0700

    add Town.swift with 2 properties

---
+++ MonsterTown/Town.swift
@@ -0,0 +1,15 @@
+//
+//  Town.swift
+//  MonsterTown
+//
+//  Created by Jeff Ripke on 9/5/17.
+//  Copyright © 2017 Jeff Ripke. All rights reserved.
+//
+
+import Foundation
+
+struct Town {
+    // properties
+    var population = 5_422
+    var numberOfStoplights = 4
+}
